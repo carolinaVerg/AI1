f7e59fde039c3c3913f05b2ea62d0a1e60e659e1
oriheldman <oheldman@gmail.com>
2018-10-23 13:12:51 +0300

    "more"

--- AI1/src/main.java
+++ AI1/src/main.java
@@ -3,13 +3,22 @@ public class main {
 
 	public static void main(String[] args) {
 		File file = new File("C:\\Users\\Oro\\Desktop\\FROMLAB\\AI1\\AI1\\src\\tests\\test1.txt"); //graph description
-		
+		int deadLine;
 		BufferedReader br;
 		String st; 
 		try {
 			br = new BufferedReader(new FileReader(file));
 			  try {
-					while ((st = br.readLine()) != null) 
+					while ((st = br.readLine()) != null) {
+						int indexOfHashtag = st.indexOf('#');
+						switch(st.charAt(indexOfHashtag +1)){
+							case 'V':
+
+							case 'E':
+							case 'D':
+							    deadLine = Integer.parseInt(st.substring(indexOfHashtag+2));
+						}
+					}
 					    System.out.println(st);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
